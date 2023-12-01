5d245cb53f48bdf40bb2c7b53d4a20413a362e73
makevs <make@texas.fail>
2023-12-01 11:38:12 +0200

    Updated gitignore

--- day1/day_1_2.py
+++ day1/day_1_2.py
@@ -1,3 +1,28 @@
+ASCII_NUMBERS = {"one": 1, "two": 2, "three": 3, "four": 4,
+                 "five": 5, "six": 6, "seven": 7, "eight": 8,
+                 "nine": 9, "zero": 0}
+
+
+def find_multiples(temprow: str, tempnum: str):
+    index_values = []
+    i = 0
+
+    while i in range(0, len(row)):
+        index = temprow.find(tempnum, i)
+        if index == -1:
+            break
+
+        if index not in index_values:
+            index_values.append(index)
+
+        i += len(tempnum) -1
+
+        if i > len(row):
+            break
+
+    return index_values
+
+
 with open("input1.txt") as file:
     file = file.read()
     file = file.split("\n")
@@ -7,16 +32,28 @@ with open("input1.txt") as file:
     for row in file:
         first_number = ""
         last_number = ""
-        print(row)
+        # print(row)
+
+        previous_index = -1
+        temp_numbers = []
+
+        for ascii_number in ASCII_NUMBERS:
+            if row.count(ascii_number) > 1:
+                for i in find_multiples(row, ascii_number):
+                    temp_numbers.append(i)
+
+            elif row.find(ascii_number) != 1:
+                temp_numbers.append(row.find(ascii_number))
+
+        print(temp_numbers)
+
         for letter in row:
             try:
                 int(letter)
                 if first_number == "":
                     first_number = int(letter)
-                    print(first_number)
                 else:
                     last_number = int(letter)
-                    print(last_number)
 
             except ValueError:
                 continue
